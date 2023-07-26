59ed5dfa867fa33f76927bb59e9eab2242e08166
hanzn-zzx <hanznzzx@126.com>
2023-07-26 13:55:46 +0800

    docs: complete config file

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -6,23 +6,17 @@ export default defineConfig({
   description: "针对小白的Linux入门文档",
   base: "/doc/",
   outDir: "../public",
+  lang: 'zh-CN',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: '文档首页', link: '/' },
       { text: 'Linux入门', link: '/wiki/introduction' },
       { text: 'deepin常用资源整理', link: 'https://deepincol.gitlab.io/deepin-src-col.html' }
     ],
 
     sidebar: [
+      { text: '文档首页', link: '/' },
       { text: '水文', link:'/home' },
-      {
-        text: 'Examples',
-        items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
     ],
 
     socialLinks: [
